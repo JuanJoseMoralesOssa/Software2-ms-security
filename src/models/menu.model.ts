c99fcc6c6e4d6d7aeb2595b47d9adde07c54497e
juan <juanjomoss@gmail.com>
2024-10-10 10:59:56 -0500

    datasource and repositories

--- src/models/menu.model.ts
+++ src/models/menu.model.ts
@@ -1,4 +1,6 @@
-import {Entity, model, property} from '@loopback/repository';
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Rol} from './rol.model';
+import {RolMenu} from './rol-menu.model';
 
 @model()
 export class Menu extends Entity {
@@ -20,6 +22,9 @@ export class Menu extends Entity {
     required: true,
   })
   comentario?: string;
+
+  @hasMany(() => Rol, {through: {model: () => RolMenu}})
+  rols: Rol[];
 
   constructor(data?: Partial<Menu>) {
     super(data);
