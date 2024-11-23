6cac90b4e24f27ecdb88debe569b6194c7ad40f0
juan <juanjomoss@gmail.com>
2024-11-22 20:21:36 -0500

    Se corrigio la url de notificaciones

--- src/config/notificaciones.config.ts
+++ src/config/notificaciones.config.ts
@@ -1,12 +1,17 @@
 export namespace NotificacionesConfig {
-  export const subject2fa: string = "Codigo de verificacion";
+  export const subject2fa: string = "Codigo de verificacion segundo factor";
   export const subjectAuditoria: string = "Asunto de auditoria";
   export const emailVerification: string = "Email Verificacion";
-  export const assignedPassword: string = "Contraseña de la cuenta de akinmueble";
-  export const urlNotificationClientRequestEmail: string = "https://localhost:5258/Notification/client-request-mail";
-  export const urlNotificationCredentialsEmail: string = "https://localhost:5258/Notification/credentials-mail";
-  export const urlNotifiPasswordRecoveryEmail: string = "https://localhost:5258/Notification/password-recovery-mail";
-  export const urlNotifications2fa: string = "https://localhost:5258/Notification/enviar-correo-2fa";
-  export const urlNotificationsSms: string = "https://localhost:5258/Notification/send-sms";
+  export const assignedPassword: string = "Contraseña de la cuenta de la aplicacion";
+  export const urlNotificationClientRequestEmail: string =
+    'https://localhost:5258/Notificaciones/client-request-mail';
+  export const urlNotificationCredentialsEmail: string =
+    'https://localhost:5258/Notificaciones/credentials-mail';
+  export const urlNotifiPasswordRecoveryEmail: string =
+    'https://localhost:5258/Notificaciones/password-recovery-mail';
+  export const urlNotifications2fa: string =
+    'https://localhost:5258/Notificaciones/enviar-correo-2fa';
+  export const urlNotificationsSms: string =
+    'https://localhost:5258/Notificaciones/send-sms';
   export const urlValidateEmailFrontend: string = "http://localhost:4200/security/validate-public-user-hash";
 }
